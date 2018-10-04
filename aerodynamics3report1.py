d29531921c4cfa9be9b2c6900cdc585a63f664db
貴裕 <takahiro@takahiro-no-MacBook-Air-3.local>
2018-10-04 13:43:30 +0900

    the first report

---
+++ aerodynamics3report1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  4 13:19:51 2018
+
+@author: takahiro
+"""
+
+class SIM3DOF:
+    def __init__(self):
+        self.U = 180
+        self.U_dt = 0
+    
